80aceafa026ff0eef8bc63b6f81a943e978085cd
HarnamSaggu <atoastytoasteronacoaster@gmail.com>
2021-12-20 23:58:02 +0000

    Algorithm cleaned

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,17 +7,14 @@ fun main() {
 fun generateBreaks(input: String): MutableList<String> {
 	val breaks = MutableList(2.0.pow(input.length - 1).toInt()) { input }
 	for (i in 0 until 2.0.pow(input.length - 1).toInt()) {
-		val binaryRep = String.format("%03d", Integer.toBinaryString(i).toInt())
-		println(binaryRep)
+		val binaryRep = String.format("%0${input.length - 1}d}", Integer.toBinaryString(i).toInt())
 		var spaceCount = 1
-		var newBreak = input
 		for (index in binaryRep.indices) {
 			if (binaryRep[index] == '1') {
-				newBreak = newBreak.substring(0, index + spaceCount) + " " + newBreak.substring(index + spaceCount)
+				breaks[i] = breaks[i].substring(0, index + spaceCount) + " " + breaks[i].substring(index + spaceCount)
 				spaceCount++
 			}
 		}
-		breaks[i] = newBreak
 	}
 
 	return breaks
