d2ba1460c246e61b8366e6a84d64043785fd3cdd
HarnamSaggu <atoastytoasteronacoaster@gmail.com>
2021-12-21 00:42:10 +0000

    FINISHED SPLITTING OF SENTENCE FINALLY++

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,13 +8,13 @@ fun generateBreaks(input: String): MutableList<MutableList<String>> {
 	val inputList = input.split("[ \t]+".toRegex()).toMutableList()
 	if (inputList.size <= 1) return mutableListOf(inputList)
 
-	val breaks = MutableList(2.0.pow(inputList.size - 1).toInt()) { (0 until inputList.size).toMutableList() }
+	val breaks = MutableList(2.0.pow(inputList.size - 1).toInt()) { inputList.indices.toMutableList() }
 
 	for (i in 0 until 2.0.pow(inputList.size - 1).toInt()) {
-		val binaryKey = String.format("%0${inputList.size - 1}d", Integer.toBinaryString(i).toInt()) // BINARY COUNTER VAL
+		val binaryKey = String.format("%0${inputList.size - 1}d", Integer.toBinaryString(i).toInt())
 		var offset = 1
 		for (cutIndex in binaryKey.indices) {
-			if (binaryKey[cutIndex] == '1') { // CUT
+			if (binaryKey[cutIndex] == '1') {
 				breaks[i] = (breaks[i].subList(0, cutIndex + offset) + mutableListOf(-1) + breaks[i].subList(cutIndex + offset, breaks[i].size)).toMutableList()
 				offset++
 			}
