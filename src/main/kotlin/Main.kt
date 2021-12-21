db356348d8189eb85d127b23526ac1c7fac73c88
HarnamSaggu <atoastytoasteronacoaster@gmail.com>
2021-12-21 01:53:06 +0000

    Got the api semi working

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
+import com.adamratzman.spotify.endpoints.pub.SearchApi
+import com.adamratzman.spotify.endpoints.pub.TrackApi
+import com.adamratzman.spotify.spotifyAppApi
+import java.io.File
+import java.io.InputStream
 import kotlin.math.pow
 
-fun main() {
-	generateBreaks("a b c d e f").forEach { println(it) }
+
+suspend fun main() {
+//	generateBreaks("a b c d e f").forEach { println(it) }
+
+	val inputStream: InputStream = File("C:/Users/theto/OneDrive/Desktop/spotify api keys.txt").inputStream()
+	val lineList = mutableListOf<String>()
+
+	inputStream.bufferedReader().forEachLine { lineList.add(it) }
+	val clientId = lineList[0]
+	val clientSecret = lineList[1]
+	val query = "karma police"
+
+	val api = spotifyAppApi(clientId, clientSecret).build()
+	SearchApi(api).searchTrack(query).forEach {
+		if (it != null) {
+			println("ITEM: ${it.name}\n\t${it.artists[0].name}")
+		}
+	}
 }
 
 fun generateBreaks(input: String): MutableList<MutableList<String>> {
@@ -36,5 +57,6 @@ fun generateBreaks(input: String): MutableList<MutableList<String>> {
 		stringBreaks[i][nextIndex] = stringBreaks[i][nextIndex].substring(0, stringBreaks[i][nextIndex].length - 1)
 	}
 
+	stringBreaks.reverse()
 	return stringBreaks
 }
